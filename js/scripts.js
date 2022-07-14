b1cd486a3015e3aa97bdff6fd3fd0f67862ea458
DESKTOP-M3EH82I\Casey <casey.haralson@gmail.com>
2022-07-14 14:11:46 -0600

    setting the default search input to have focus and setting the search buttons to skip during tabbing through the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
-/*!
-* Start Bootstrap - Creative v7.0.6 (https://startbootstrap.com/theme/creative)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
 
 window.addEventListener('DOMContentLoaded', event => {
 
+
+    // set the default input to have focus
+    document.body.querySelector('.searchgroup.default input').focus();
+
+
+
+    // keep all of the search boxes up-to-date with the current search text
     var searchGroupInputs = document.body.querySelectorAll('.searchgroup input');
 
     var searchTextInputHandler = function (e) {
@@ -29,7 +28,7 @@ window.addEventListener('DOMContentLoaded', event => {
         
 
 
-
+    // handle forms that have additional data that needs appended to the query
     var queryAppendForms = document.body.querySelectorAll('.searchgroup form[data-query-append]');
 
     var queryAppendFormSubmitHandler = function (e) {
@@ -49,57 +48,5 @@ window.addEventListener('DOMContentLoaded', event => {
         item.addEventListener('submit', queryAppendFormSubmitHandler);
     });
 
-
-
-    /*
-
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
-    };
-
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
-
-*/
 
 });
